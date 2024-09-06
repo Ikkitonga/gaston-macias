164ca8dd5ffba425e89f8d36d88f98ed8c478e16
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-06 18:52:29 -0300

    se agrego presentacion y footer

--- gaston-macias/src/pages/Home.jsx
+++ gaston-macias/src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
+import SeccionPresentacion from "../components/seccionPresentacion/SeccionPresentacion";
 
 import "./home.css";
 
 const Home = () => {
     return <>
         <Navbar />
+        <SeccionPresentacion />
+        <Footer />
     </>
 }
 
