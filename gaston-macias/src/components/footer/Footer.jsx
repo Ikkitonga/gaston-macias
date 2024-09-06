48689ca1b274f829077fa7662ee8177a7ce0dca0
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-06 18:53:22 -0300

    componente footer

---
+++ gaston-macias/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./footer.css";
+import { FaLinkedinIn, FaGithub, FaWhatsapp } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <h2 className="logo">GM</h2>
+            <p className="texto__footer">GRACIAS POR SU VISITA</p>
+            <div className="iconos-sociales">
+                <FaLinkedinIn />
+                <FaGithub />
+                <FaWhatsapp />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
