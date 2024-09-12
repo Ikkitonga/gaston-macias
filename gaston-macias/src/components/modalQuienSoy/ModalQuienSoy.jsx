ce2d0a25b5d26ceeb7506ea760dd569d4cbcea88
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-11 22:23:35 -0300

    estilos colores y acentuar texto

--- gaston-macias/src/components/modalQuienSoy/ModalQuienSoy.jsx
+++ gaston-macias/src/components/modalQuienSoy/ModalQuienSoy.jsx
@@ -1,4 +1,5 @@
 import "./ModalQuienSoy.css";
+import { IoClose } from "react-icons/io5";
 
 
 const ModalQuienSoy = ({show, handleClose}) => {
@@ -8,25 +9,25 @@ const ModalQuienSoy = ({show, handleClose}) => {
     return (
         <article className="modal is-open">
             <div className="modal-container">
-            <button className="modal-close" onClick={handleClose}>X</button>
-                <div className="modal-header">
+            <button className="modal-close" onClick={handleClose}><IoClose /></button>
+                <div className="modal-header quienSoy-header">
                     <h2>Quien Soy</h2>
                 </div>
-                <div className="modal-datos">
-                    <p>¡Bienvenido a mi portafolio! Soy Gastón Ezequiel Macías, Técnico Electrónico especializado en 
+                <div className="modal-datos datos-quienSoy">
+                    <p>¡Bienvenido a mi portafolio! Soy <span className="resalte-quienSoy">Gastón Ezequiel Macías</span>, Técnico Electrónico especializado en 
                         telecomunicaciones. Desde joven me apasiona la tecnología, y a lo largo de los últimos años, 
-                        me he sumergido en el desarrollo frontend. He adquirido mis habilidades a través de cursos online 
+                        me he sumergido en el <span className="resalte-quienSoy">Desarrollo Frontend</span>. He adquirido mis habilidades a través de cursos online 
                         y estudios autodidactas, siempre buscando mejorar y aprender más en este apasionante campo.
 
                         Además de la tecnología, soy un apasionado de los videojuegos, especialmente de World of Warcraft, 
-                        donde disfruto tanto de la estrategia como del trabajo en equipo. Esta pasión me ayuda a mantenerme creativo 
+                        donde disfruto tanto de la estrategia como del <span className="resalte-quienSoy">trabajo en equipo</span>. Esta pasión me ayuda a mantenerme creativo 
                         y enfocado, cualidades que también aplico en mis proyectos de desarrollo frontend.
 
                         Mi objetivo es continuar creciendo como desarrollador frontend y contribuir con proyectos 
                         que impulsen la innovación y el diseño eficiente.
 
                         Si te interesa saber más sobre mí o colaborar en un proyecto, no dudes en contactarme. 
-                        ¡Estoy siempre abierto a nuevas oportunidades!
+                        <span className="resalte-quienSoy">¡Estoy siempre abierto a nuevas oportunidades!</span>
                     </p>
                 </div>
             </div>
