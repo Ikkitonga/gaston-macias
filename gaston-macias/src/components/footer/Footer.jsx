c21363d530dbbbedc728dde0335122a72c40a084
gastonMacias <gastonezequielmacias@gmail.com>
2024-09-17 16:49:26 -0300

    estilos a los iconos sociales y se les dio un anchor a cada uno

--- gaston-macias/src/components/footer/Footer.jsx
+++ gaston-macias/src/components/footer/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     return (
         <footer className="footer">
             <h2 className="logo">GM</h2>
-            {/* <p className="texto__footer">GRACIAS POR SU VISITA</p> */}
+            
             <div className="iconos-sociales">
-                <FaLinkedinIn />
-                <FaGithub />
-                <FaWhatsapp />
+                <a href="https://www.linkedin.com/in/gastonezequielmacias" target="_blank"><FaLinkedinIn /></a>
+                <a href="https://github.com/Ikkitonga" target="_blank"><FaGithub /></a>
+                <a href="https://wa.me/54911159362462" target="_blank"><FaWhatsapp /></a>
             </div>
         </footer>
     )
